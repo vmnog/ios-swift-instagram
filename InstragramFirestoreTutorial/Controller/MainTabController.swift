6c60501b46888045b072ed0bef07a931d5f8755c
Victor Nogueira <victor.nogueira@aitia.com.br>
2021-03-04 12:00:22 -0300

    config: initial commit

---
+++ InstragramFirestoreTutorial/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  InstragramFirestoreTutorial
+//
+//  Created by Victor Nogueira on 04/03/21.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
---
+++ InstragramFirestoreTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstragramFirestoreTutorial
+//
+//  Created by Victor Nogueira on 04/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
