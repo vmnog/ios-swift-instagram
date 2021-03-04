ec2306388d55638d16d3c70295e2f44c8b9d4146
Victor Nogueira <victor.nogueira@aitia.com.br>
2021-03-04 16:21:31 -0300

    feat: add username to the post profile

--- InstragramFirestoreTutorial/View/FeedCell.swift
+++ InstragramFirestoreTutorial/View/FeedCell.swift
@@ -20,6 +20,21 @@ class FeedCell: UICollectionViewCell {
         return userProfilePicture
     }()
     
+    
+    // Defines username button
+    private lazy var usernameButton: UIButton = {
+        let profileButton = UIButton(type: .system)
+        
+        // Post username
+        profileButton.setTitle("venom", for: .normal)
+        profileButton.setTitleColor(.black, for: .normal)
+        profileButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+        
+        // Press event calls didTapUsername handler function
+        profileButton.addTarget(self, action: #selector(didTapUsername), for: .touchUpInside)
+        return profileButton
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
@@ -39,10 +54,21 @@ class FeedCell: UICollectionViewCell {
         // Make image a circle
         profileImageView.layer.cornerRadius = 40 / 2
         
+        // Adds username button to the post
+        addSubview(usernameButton)
+        usernameButton.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
+        
     }
     
     // Required default constructor
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Actions
+    
+    // Handler -> When user clicks posts owner username
+    @objc func didTapUsername() {
+        print("clicou")
     }
 }
