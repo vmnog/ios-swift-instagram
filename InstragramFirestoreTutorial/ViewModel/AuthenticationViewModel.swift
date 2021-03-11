34fa855bc968cfc06a92fcace5d5c004f576a37b
Victor Nogueira <victor.nogueira@aitia.com.br>
2021-03-10 22:33:33 -0300

    feat: implements authenticationViewModel for registration validation

--- InstragramFirestoreTutorial/ViewModel/AuthenticationViewModel.swift
+++ InstragramFirestoreTutorial/ViewModel/AuthenticationViewModel.swift
@@ -7,7 +7,13 @@
 
 import UIKit
 
-struct LoginViewModel {
+protocol AuthenticationViewModel {
+    var formIsValid: Bool { get }
+    var buttonBackgroundColor: UIColor { get }
+    var buttonTitleColor: UIColor { get }
+}
+
+struct LoginViewModel: AuthenticationViewModel {
     var email: String?
     var password: String?
     
@@ -24,6 +30,26 @@ struct LoginViewModel {
     }
 }
 
-struct RegistrationViewModel {
+struct RegistrationViewModel: AuthenticationViewModel {
+    var email: String?
+    var password: String?
+    var username: String?
+    var fullname: String?
+    
+    var formIsValid: Bool {
+        return
+            email?.isEmpty == false &&
+            password?.isEmpty == false &&
+            username?.isEmpty == false &&
+            fullname?.isEmpty == false
+    }
+    
+    var buttonBackgroundColor: UIColor {
+        return formIsValid ? #colorLiteral(red: 0.4374820559, green: 0.2786109558, blue: 0.770463198, alpha: 0.5977632705) : #colorLiteral(red: 0.4374820559, green: 0.2786109558, blue: 0.770463198, alpha: 0.1479826627)
+    }
+    
+    var buttonTitleColor: UIColor {
+        return formIsValid ? .white : UIColor(white: 1, alpha: 0.5)
+    }
     
 }
